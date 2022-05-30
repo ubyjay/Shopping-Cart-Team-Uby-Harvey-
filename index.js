5a886c4333ed9f5b25bb14eede511517b0164fed
Benjamin Akpabot <akpabotubong@gmail.com>
2022-05-30 19:24:44 +0100

    updated the Product on cart Menu

--- index.js
+++ index.js
@@ -34,7 +34,12 @@ function ready (){
         var input = quantityInputs[i]
         input.addEventListener('change',quantityChanged); 
     }
-    
+    // ADD CART
+    var addCart = document.getElementsByClassName('add-cart')
+    for (var i = 0; i < addCart.length; i++){
+        var button = addCart[i];
+        button.addEventListener('click',addCartClicked) 
+    }
 }
 // REMOVE ITEMS FROM CART(making fuction removeCartItem)
 function removeCartItem (event){
@@ -49,6 +54,46 @@ function quantityChanged(){
         input.value = 1;
     }
     updateTotal();
+}
+//Add to Cart
+function addCartClicked(event){
+    var button = event.target
+    var shopProducts = button.parentElement
+    var title = shopProducts.getElementsByClassName('product-title')[0].innerText
+    var price = shopProducts.getElementsByClassName('price')[0].innerText
+    var productImg = shopProducts.getElementsByClassName('product-img')[0].src
+    addProductToCart(title, price, productImg);
+    updateTotal();
+}
+function addProductToCart(title, price, productImg){
+    var cartShopBox = document.createElement('div');
+        cartShopBox.classList.add('cart-box')
+    var cartItems = document.getElementsByClassName('cart-content')[0]
+    var cartItemsNames = cartItems.getElementsByClassName('cart-product-title')
+    for (var i = 0; i < cartItemsNames.length; i++){
+        if(cartItemsNames[i].innerText == title ){
+            alert("you have already add this item to cart");
+            return;
+        }
+        
+    }
+    
+
+
+var cartBoxContent = `
+                    <img src="${productImg}" alt="" class="cart-img">
+                    <div class="detail-box">
+                        <div class="cart-product title">${title}</div>
+                        <div class="cart-price">${price}</div>
+                        <input type="number" value="1" class="cart-quantity">
+                        <!-- REMOVE CART -->
+                    </div>
+                    <i class='bx bxs-trash-alt cart-remove'></i>
+`
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener('click',removeCartItem);
+cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change',quantityChanged);
 }
 // UPdate Total
 function updateTotal (){
