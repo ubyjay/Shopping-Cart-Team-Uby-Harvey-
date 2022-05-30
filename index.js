4ce8e63618f6136a9c8f92e4acd13d5f4c6c6bdc
Benjamin Akpabot <akpabotubong@gmail.com>
2022-05-30 14:12:19 +0100

    updated the input status and rounded it

--- index.js
+++ index.js
@@ -28,12 +28,44 @@ function ready (){
         var button = removeCartButtons[i]
         button.addEventListener('click',removeCartItem)
     }
+    //QUantity Changes
+    var quantityInputs = document.getElementsByClassName('cart-quantity') 
+    for (var i = 0; i < quantityInputs.length; i++){
+        var input = quantityInputs[i]
+        input.addEventListener('change',quantityChanged); 
+    }
+    
 }
 // REMOVE ITEMS FROM CART(making fuction removeCartItem)
 function removeCartItem (event){
     var buttonClicked = event.target
     buttonClicked.parentElement.remove()
-
+    updateTotal()
+}
+// Quantity changes
+function quantityChanged(){
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0){
+        input.value = 1;
+    }
+    updateTotal();
 }
 // UPdate Total
+function updateTotal (){
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    var cartBoxes = cartContent.getElementsByClassName('cart-box');
+    var initial = 0;
+    for(var i = 0; i < cartBoxes.length; i++){
+        var cartBox = cartBoxes[i]
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0]
+        var price = parseFloat(priceElement.innerText.replace("$", ""))
+        var quantity = quantityElement.value
+        initial = initial + (price * quantity)
+        total = initial.toFixed(2);
 
+        // if price contains some cents Value
+
+        document.getElementsByClassName('total-price')[0].innerHTML = "$" + total;
+    }
+}
